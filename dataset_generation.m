8162375e3d2ec00a9612b52aa8613cdea98bc8ac
David Krause <s8090801@tu-dresden.de>
2022-01-19 11:58:18 +0100

    dataset generation finished

--- dataset_generation.m
+++ dataset_generation.m
@@ -5,7 +5,8 @@
 number_of_modes = 5;    %option: 3 or 5
 number_of_data = 10000;
 image_size = 32;    % resolution 32x32
- 
+split = floor(0.8 * number_of_data);
+
 %% generation of complex mode weights and label vector - Aufgabe 1
 
 % 1. create random amplitude weights. The weights of amplitude should be normalized.
@@ -15,22 +16,29 @@ amp_ws = amp_ws ./ vecnorm(amp_ws);
 % 2. create random phase amplitude. (Using realtive phase difference)
 phs_ws = wrapToPi(rand(number_of_modes, number_of_data) * 2*pi);
 rel_phs_ws = phs_ws - phs_ws(1, :);
-% what is with mode 1
-rel_phs_ws = cos(rel_phs_ws);
 
 % 3. complex mode weights vector
+cplx_ws = amp_ws .* exp(1i*rel_phs_ws);
 
 % 4. normalize cos(phase) to (0,1)
+rel_phs_ws = cos(rel_phs_ws(2:end, :));
+norm_phs_ws = normalization(rel_phs_ws, 0, 1); 
 
 % 5. combine amplitude and phase into a label vector (1,2N-1)
+labels = [amp_ws; norm_phs_ws];
 
 % 6. split complex mode weights vector and label vector into Training,
 % validation and test set. 
+train_labels = labels(:, 1:split);
+test_labels = labels(:, split+1:end);
 
 %% create image data - Aufgabe 2 
 % use function mmf_build_image()
-
+images = mmf_build_image(number_of_modes, image_size, number_of_data, cplx_ws);
 
 %% save dataset
-
+train_imgs = images(:, :, 1, 1:split);
+test_imgs = images(:, :, 1, split+1:end);
+name = sprintf('%d-modes_%dx%d_%dk.mat', number_of_modes, image_size, image_size, floor(number_of_data/1000));
+save(fullfile('data', name), 'train_imgs', 'train_labels', 'test_imgs', 'test_labels')
 
--- mmf_build_image.m
+++ mmf_build_image.m
@@ -1,22 +1,31 @@
-function [Image_data] = mmf_build_image(number_of_modes,image_size,number_of_data, complex_weights_vector)
+function [Image_data] = mmf_build_image(number_of_modes, image_size, number_of_data, complex_weights_vector)
 %% load complex mode distribution
-% load the complex distrbutions 
-
+% load the complex distrbutions
+if number_of_modes == 5
+    load('mmf_5modes_32.mat')
+else
+    errID = 'myComponent:inputError';
+    msgtext = sprintf('No file for %d modes', number_of_modes);
+    ME = MException(errID,msgtext);
+    throw(ME);
+end
 
 %% create images
 % define a variable for Image data with dimension (image size, image size, 1, n)
 fprintf("Start to generate the mode distribution......\n");
-
-for index_number=1:number_of_data
+Image_data = zeros(image_size, image_size, 1, number_of_data);
+for index_number = 1:number_of_data
     % 1. define a variable for single image with resolution (image size,image size)
+    weights = complex_weights_vector(:, index_number);
     
     % 2. generation of complex field distribution 
-
-    % 3. abstract Amplitude distribution 
+    cplx_field = sum(mmf_5modes_32 .* shiftdim(weights, -2), 3);
     
+    % 3. abstract Amplitude distribution 
     % 4. normalization the amplitude distribution to (0,1)
     %    using normalization(image, minValue, maxValue)
-    
+    norm_field = normalization(abs(cplx_field), 0, 1);
+    Image_data(:, :, 1, index_number) = norm_field;   
     
 end
 fprintf("The image data has been generated.\n");
